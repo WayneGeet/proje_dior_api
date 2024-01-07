8c6e0da3c93299cad6231851d2106c77e6b76caa
Wynne Gitau <wayne.geet64@gmail.com>
2024-01-07 22:32:49 +0300

    Revert "initial commit"
    This reverts commit 39005fa0f4f8166d8ca876b084308657811d81fb.

--- user_profile/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Profile
-from rest_framework.validators import ValidationError
-
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = "__all__"
